68b7489676028df5efb6e6950b768db4b70be8f9
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-01-09 22:28:03 -0600

    background using tweened and circle using spring from svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,47 @@
 <script>
-	
+	import { spring, tweened } from 'svelte/motion'
+	import { cubicOut } from 'svelte/easing';
+
+	let mouse = spring({ x: 0, y: 0 }, {
+		stiffness: 0.00001,
+		damping: .001
+	})
 
 	let canvasWidth
 	let canvasHeight
-	let mouse = { x: 0, y: 0 }
-	let hue = 50
+	let backgroundHue = tweened(50, {
+		duration: 5000,
+		easing: cubicOut
+	})
+	let forgroundHue = tweened(50, {
+		duration: 5000,
+		easing: cubicOut
+	})
+
 	const hueMaxValue = 360
 
 	const handleMouseMove = (e) => {
-		mouse.x = e.clientX
-		mouse.y = e.clientY
-
-		hue = hueMaxValue * mouse.x /canvasWidth
+		mouse.set({ x: e.clientX, y: e.clientY })
+	
+		backgroundHue.set(hueMaxValue * $mouse.x / canvasWidth)
+		forgroundHue.set(hueMaxValue * $mouse.y / canvasHeight)
 	}
 </script>
 
 <style>
-	h1 {
-		color: red;
-	}
-	canvas {
-		height: 100vh;
-		width: 100vw;
-		/* background-color: red; */
-		background-color: hsl(var(--hue), 50%, 50%);
-	}
+	div { width: 100%; height: 100% }
+	svg { width: 100%; height: 100%; background-color: hsl(var(--backgroundHue), 50%, 50%); }
+	circle { fill: hsl(var(--forgroundHue), 50%, 50%); }
 </style>
 
-<h1>x = {mouse.x} y = {mouse.y}</h1>
-<p>height = {canvasHeight} width = {canvasWidth}</p>
-<p>hue = {hue}</p>
-<canvas 
-	style="--hue:{hue}"
-	on:mousemove={handleMouseMove}
+<div
 	bind:clientHeight={canvasHeight}
 	bind:clientWidth={canvasWidth}
 >
-</canvas>
+	<svg
+		style="--backgroundHue:{$backgroundHue}"
+		on:mousemove="{handleMouseMove}"
+	>
+		<circle style="--forgroundHue:{$forgroundHue}" cx={$mouse.x} cy={$mouse.y} r={500} fill-opacity="50%" />
+	</svg>
+</div>
