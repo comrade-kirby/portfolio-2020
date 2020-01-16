0ca069c38c17ee82d2e580f87dcf3799105e315a
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-01-16 14:27:01 -0600

    remove unneeded writables from index

--- src/Index.svelte
+++ src/Index.svelte
@@ -1,36 +1,36 @@
 <script>
-  import { writable } from 'svelte/store'
+  import { spring, tweened } from 'svelte/motion'
   import { onMount } from 'svelte'
 
   import { longestScreenDimension } from './stores.js'
   import P5Canvas from './P5Canvas.svelte'
 
-  let cubeSize = writable(0)
+  let cubeSize = 0
   let isOpen = false
-  let sizeChanged = writable(false)
+  let sizeChanged = false
 
   const sketch = (p5) => {
 	  p5.setup = () => {
-      const canvas = p5.createCanvas($cubeSize, $cubeSize, p5.WEBGL)
+      const canvas = p5.createCanvas(cubeSize, cubeSize, p5.WEBGL)
       canvas.parent('index-holder')
       p5.colorMode(p5.HSL, 360, 100, 100, 100)
 	  }
 
 	  p5.draw = () => {
-      if ($sizeChanged) {
-        p5.resizeCanvas($cubeSize, $cubeSize)
-        sizeChanged.set(false)
+      if (sizeChanged) {
+        p5.resizeCanvas(cubeSize, cubeSize)
+        sizeChanged = false
       }
       p5.background(360, 100, 0, 0)
       p5.rotateX(p5.frameCount * .01)
       p5.rotateY(p5.frameCount * .02)
       p5.fill(0, 0, 0, 50)
       p5.stroke('white')
-      p5.box($cubeSize / 2)
+      p5.box(cubeSize / 2)
     }
 
     p5.windowResized = () => {
-      p5.resizeCanvas($cubeSize, $cubeSize)
+      p5.resizeCanvas(cubeSize, cubeSize)
     }
   }
 
@@ -41,14 +41,14 @@
 
   const resize = (size) => {
     if (!isOpen) {
-      sizeChanged.set(true)
-      cubeSize.set($longestScreenDimension * size)
+      sizeChanged = true
+      cubeSize = $longestScreenDimension * size
     }
   }
 
   onMount(async () => {
     await $longestScreenDimension
-    cubeSize.set($longestScreenDimension * .15)
+    cubeSize = $longestScreenDimension * .15
   })
 </script> 
 
@@ -63,7 +63,7 @@
 
 <div 
   id='index-holder'
-  style='--cubeSize:{`${$cubeSize}px`}'
+  style='--cubeSize:{`${cubeSize}px`}'
   on:mousedown={() => {resize(.14)}}
   on:mouseup={open}
   on:mouseover={() => resize(.16)}
