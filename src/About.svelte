e6d865cfc1328a7fcbe01ef42ab39463a24cac8b
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-01-23 14:37:01 -0600

    create about page w/ layout

--- src/About.svelte
+++ src/About.svelte
@@ -1,43 +1,70 @@
 <script>
   import { fade } from 'svelte/transition'
+  import P5Canvas from './P5Canvas.svelte'
+  
+  let aboutHeight
+  let aboutWidth
+
+  const drawContainer = (p5) => {
+    p5.fill(0, 0, 100, 90)
+    p5.noStroke()
+    p5.rect(0, 0, aboutWidth, aboutHeight);
+  }
+
+  const drawTitle = (p5) => {
+    let title = 'about us'
+    p5.textSize(24)
+    p5.textAlign(p5.LEFT, p5.TOP)
+    p5.text(title, 20, 20, aboutWidth - 20, 30)
+  }
+
+  const drawContent = (p5) => {
+    let content = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam in nesciunt facilis. Aspernatur officia ratione laborum saepe aliquam, ut perspiciatis dolores a vitae facere veritatis suscipit, quas ipsum, ullam voluptate!'
+    p5.textSize(18)
+    p5.textAlign(p5.RIGHT, p5.BOTTOM)
+    p5.text(content, 100, 40, aboutWidth - 120, aboutHeight - 60)
+  }
+
+  const drawText = (p5) => {
+    p5.textSize(24)
+    p5.erase()
+    drawTitle(p5)
+    drawContent(p5)
+    p5.noErase()
+    p5.fill(0, 0, 0, 15)
+    drawContent(p5)
+    drawTitle(p5)
+  }
+
+  const sketch = (p5) => {
+	  p5.setup = () => {
+      const canvas = p5.createCanvas(aboutWidth, aboutHeight)
+      canvas.parent('about-holder')
+      p5.colorMode(p5.HSL, 360, 100, 100, 100)
+      drawContainer(p5)
+      drawText(p5)
+	  }
+
+    p5.windowResized = () => {
+      p5.resizeCanvas(aboutWidth, aboutHeight)
+      drawContainer(p5)
+      drawText(p5)
+    }
+  }
 </script>
-
+  
 <style>
-  .knockout-box {
-    width: 50%;
+  #about-holder {
     height: 50%;
+    width: 50%;
     min-width: 300px;
-  }
-
-  svg #overlay {
-    fill: white;
-    opacity: .9;
-  }
-  svg #text {
-    font-size: 40px;
-  }
-
-  svg #mask-container {
-    fill: white;
-    mask: url(#box-mask);
-  }
-  svg #text-darken {
-    fill: black;
-    opacity: .1;
   }
 </style>
 
-<svg transition:fade class='knockout-box'>
-  <defs>
-    <mask id='box-mask' x='0' y='0' width='100%' height='100%'>
-      <rect id='overlay' x='0' y='0' width='100%' height='100%' />
-      <text id='text' text-anchor='end' x='100%' y='100%' dx='-20' dy='-100' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi fugiat totam esse nulla aspernatur facere ex, quaerat tenetur repellat, sequi fuga libero. Deleniti exercitationem facilis repellendus quas ipsa? Alias, modi.</text>
-      <!-- <svg viewBox='0 0 36 36' height='36' width='100%' preserveAspectRatio='xMaxYMin meet'>
-        <path d='M6 19h12v2H6z' />
-        <path fill='none' d='M0 0h24v24H0V0z'/>
-      </svg> -->
-    </mask>
-  </defs>
-  <rect id='text-darken' x='0' y='0' width='100%' height='100%' />
-  <rect id='mask-container' x='0' y='0' width='100%' height='100%' />
-</svg>
+<div 
+  id='about-holder'
+  bind:clientHeight={aboutHeight}
+	bind:clientWidth={aboutWidth}
+  >
+  <P5Canvas sketch={sketch} />
+</div>
--- src/Home.svelte
+++ src/Home.svelte
@@ -12,25 +12,26 @@
   }
 
   const drawName = (p5) => {
-    p5.textAlign(p5.RIGHT)
+    p5.textSize(40)
+    p5.textAlign(p5.RIGHT, p5.BOTTOM)
     p5.text('max', homeWidth - 20, homeHeight - 100)
     p5.text('houston', homeWidth - 20, homeHeight - 60)
     p5.text('oppenheimer', homeWidth - 20, homeHeight - 20)
   }
 
   const drawTitle = (p5) => {
-    p5.textAlign(p5.LEFT)
-    p5.text('creative', 20, 60)
-    p5.text('web', 20, 100)
+    p5.textSize(24)
+    p5.textAlign(p5.LEFT, p5.TOP)
+    p5.text('creative', 20, 20)
+    p5.text('web', 20, 44)
   }
 
   const drawText = (p5) => {
-    p5.textSize(40)
     p5.erase()
     drawName(p5)
     drawTitle(p5)
     p5.noErase()
-    p5.fill(0, 0, 0, 10)
+    p5.fill(0, 0, 0, 15)
     drawName(p5)
     drawTitle(p5)
   }
